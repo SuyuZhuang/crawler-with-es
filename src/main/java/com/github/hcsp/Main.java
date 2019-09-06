e24a14caf7c9a39afe865231aea93e9d640bf997
zhuangsuyuHP <zhuangsuyu1993@163.com>
2019-09-07 00:49:47 +0800

    使用H2数据库并进行重构

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,5 +1,6 @@
 package com.github.hcsp;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -9,40 +10,99 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Main {
-    public static final OkHttpClient client = new OkHttpClient();
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String JDBC_URL = "jdbc:h2:file:E:\\study\\xiedaimala\\gitpractice\\crawler-with-es\\news";
 
-    public static void main(String[] args) throws IOException {
-        // 待处理的链接池
+
+    private static List<String> loadUrlsFromDatabase(Connection connection, String sql) throws SQLException {
         List<String> linkPool = new ArrayList<>();
-        // 已处理的链接池
-        Set<String> processedLinks = new HashSet<>();
+        try (PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                linkPool.add(resultSet.getString(1));
+            }
+        }
+        return linkPool;
+    }
 
-        linkPool.add("https://sina.cn");
-        while (!linkPool.isEmpty()) {
+    @SuppressFBWarnings(value = "DMI_CONSTANT_DB_PASSWORD")
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager.getConnection(JDBC_URL, USER_NAME, PASSWORD);
+
+        while (true) {
+            List<String> linkPool = loadUrlsFromDatabase(connection, "select link from LINKS_TO_BE_PROCESSED");
+            if (linkPool.isEmpty()) {
+                break;
+            }
+
             String link = linkPool.remove(linkPool.size() - 1);
+            deleteLinkFromDatabase(connection, link, "delete from LINKS_TO_BE_PROCESSED where LINK=?");
 
-            if (processedLinks.contains(link)) {
+            if (!isLinkProcessed(connection, link)) {
                 continue;
             }
 
-            if (link.startsWith("//")) {
-                link = "https:" + link;
-            }
-
             if (isInterestingLink(link)) {
-                Document doc = getAndParceHtml(link);
-                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+                Document doc = getAndParseHtml(link);
+                parseUrlsFromPageAndStoreIntoDatabase(connection, doc);
                 storeInToDatabaseIfItIsNewsPage(doc);
-                processedLinks.add(link);
+                insertLinkIntoDatabase(connection, link, "insert into LINKS_ALREADY_PROCESSED(link) values (?)");
             }
         }
     }
+
+    private static void deleteLinkFromDatabase(Connection connection, String link, String sql) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, link);
+            statement.executeUpdate();
+        }
+    }
+
+    private static void parseUrlsFromPageAndStoreIntoDatabase(Connection connection, Document doc) throws SQLException {
+        for (Element aTag : doc.select("a")) {
+            String href = aTag.attr("href");
+            if (href.startsWith("//")) {
+                href = "https:" + href;
+            }
+            insertLinkIntoDatabase(connection, href, "insert into LINKS_TO_BE_PROCESSED(link) values (?)");
+        }
+    }
+
+    private static boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        ResultSet resultSet = null;
+        try (PreparedStatement statement = connection.prepareStatement("select link from LINKS_ALREADY_PROCESSED where LINK=?")) {
+            statement.setString(1, link);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                return true;
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+        return false;
+    }
+
+    private static void insertLinkIntoDatabase(Connection connection, String link, String sql) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, link);
+            statement.executeUpdate();
+        }
+    }
+
 
     private static void storeInToDatabaseIfItIsNewsPage(Document doc) {
         ArrayList<Element> articleTags = doc.select("article");
@@ -53,7 +113,7 @@ public class Main {
         }
     }
 
-    private static Document getAndParceHtml(String link) {
+    private static Document getAndParseHtml(String link) {
         Document doc = null;
         Request request = new Request.Builder()
                 .url(link)
